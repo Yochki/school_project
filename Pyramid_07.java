18d73a62ebb0e2f67f779c92c04b309948c649d1
Yochki <71455712+Yochki@users.noreply.github.com>
2020-10-20 22:13:21 +0300

    Add files via upload

---
+++ Pyramid_07.java
@@ -0,0 +1,19 @@
+package ex07;
+
+import java.util.Scanner;
+
+public class Pyramid_07 {
+	public static void main(String[] args) {		
+        Scanner sc = new Scanner(System.in);
+        int numberOfRows = sc.nextInt();
+        for( int i = 0; i < numberOfRows; i++) {
+        	for( int j = numberOfRows - i; j > 0;  j--) {
+        		System.out.print( j + " ");
+        	}
+        for( int j = 2; j <= numberOfRows - i; j++) {
+        	System.out.print(j + " ");
+        }
+        System.out.println();
+        }       		
+	}
+}
